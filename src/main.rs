e9a956fa5e469b00dd8e437995ebdc9bf528d2cd
Tom Andrews <git@tomandrews.co.uk>
2016-08-18 09:41:38 +0100

    Get value from memory or registry

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,17 @@ use std::io::Cursor;
 use std::path::Path;
 use byteorder::{LittleEndian, ReadBytesExt};
 
+fn get_value(val:usize, memory:&Vec<u16>, registry: &[u16]) -> usize {
+    let reg_loc = val % 32768;
+    let res:usize;
+
+    if val > 32768 {
+        res = registry[reg_loc] as usize;
+    } else {
+        res = memory[val] as usize;
+    }
+    res
+}
 
 fn main() {
     let path = Path::new("challenge/challenge.bin");
@@ -17,6 +28,7 @@ fn main() {
 
     let mut buffer = [0u8; 2];
     let mut memory:Vec<u16> = Vec::new();
+    let registry = [0u16; 8];
     let mut count = file.read(&mut buffer).unwrap();
     while count > 0 {
         let val = Cursor::new(buffer).read_u16::<LittleEndian>().unwrap();
@@ -29,12 +41,19 @@ fn main() {
     loop {
         let op = memory[pointer];
         if op == 0u16 {
+            // HALT
             break;
+        } else if op == 6u16 {
+            // JMP
+            pointer += 1;
+            pointer = get_value(pointer, &memory, &registry);
         } else if op == 21u16 {
             // NOOP
         } else if op == 19u16 {
+            // PRINT
             pointer += 1;
-            let char_val = (memory[pointer] as u8) as char;
+            let value = get_value(pointer, &memory, &registry);
+            let char_val = (value as u8) as char;
             print!("{}", char_val.to_string());
         }
 
